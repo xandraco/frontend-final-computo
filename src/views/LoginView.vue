7c602455b353bc1d73a1becdcd7325aa5cab553b
xandraco <andraco7x7@outlook.com>
2024-05-25 00:08:29 -0600

    finished the login page structure and added the logo

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container>
-    <v-row></v-row>
     <v-row>
-      <loginCard></loginCard>
+      <v-col>
+        <v-img src="https://tse3.mm.bing.net/th/id/OIG1.f9tYtq9hY4fP0l4_.VKi?pid=ImgGn" cover />
+      </v-col>
+      <v-col>
+        <loginCard></loginCard>
+      </v-col>
     </v-row>
   </v-container>
 </template>
